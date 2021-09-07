5c297b17ff3bfa206ba3b4901df0ba0ab2b94d28
Zaid <mohdzaid88798@gmail.com>
2021-09-07 15:08:23 +0530

    Added variables

---
+++ src/helloWorld.java
@@ -0,0 +1,14 @@
+/*
+My first Program
+ */
+
+public class helloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World !!!");
+        int num = 10;
+        System.out.println("Number = "+num);
+        num = 2*num;
+        System.out.println("Value of num *2 "+num);
+    }
+}
