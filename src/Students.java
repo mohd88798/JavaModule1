2fde68125b942783bd404258df36684f1be89259
Zaid <mohdzaid88798@gmail.com>
2021-09-20 19:43:05 +0530

    added marks and updated Students

--- src/Students.java
+++ src/Students.java
@@ -1,23 +1,12 @@
-
-class marks{
-    double marks1, marks2, marks3, marks4, marks5, average;
-
-    double average(){
-
-        average=(marks1+marks2+marks3+marks4+marks5)/5;
-        return average;
-    }
-}
-
 public class Students {
     public static void main(String[] args) {
+        marks z = new marks(80,80,80,80,80);
 
-        marks a = new marks();
-        a.marks1=80;
-        a.marks2=80;
-        a.marks3=80;
-        a.marks4=80;
-        a.marks5=80;
-        a.average();
+        double average;
+
+        average=(z.marks1 + z.marks2 + z.marks3 + z.marks4 + z.marks5) / 5;
+
+        System.out.println("Average = "+average);
     }
+
 }
---
+++ src/marks.java
@@ -0,0 +1,18 @@
+class marks {
+    double marks1, marks2, marks3, marks4, marks5, average;
+
+//    double average() {
+//
+//        average = (marks1 + marks2 + marks3 + marks4 + marks5) / 5;
+//
+//        return average;
+//    }
+
+    marks(double a, double b, double c, double d, double e) {
+        marks1 = a;
+        marks2 = b;
+        marks3 = c;
+        marks4 = d;
+        marks5 = e;
+    }
+}
